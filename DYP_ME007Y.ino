c3d9b13df1c6d3e36e37afc09b387fb46bc97184
barneyman <barnaby.james.flint@gmail.com>
2018-05-17 16:38:37 +1000

    uses softwareserial and circQ

--- DYP_ME007Y.ino
+++ DYP_ME007Y.ino
@@ -5,12 +5,12 @@
 #define _TX_PIN		D6
 
 
-SoftwareSerial dyp(_RX_PIN,_TX_PIN);
+SoftwareSerial dyp(_RX_PIN, _TX_PIN);
 
 void setup()
 {
 	delay(2000);
-  /* add setup code here */
+	/* add setup code here */
 	Serial.begin(9600);
 	Serial.println("Running ...");
 
@@ -18,17 +18,7 @@ void setup()
 
 }
 
-#define MAX_GRAB	32
-
-struct 
-{
-	unsigned writeCursor, size;
-	byte buffer[MAX_GRAB];
-	unsigned calculated[MAX_GRAB];
-} serialData = { 0, MAX_GRAB };
-
-
-circQueueT<32, 4> blah;
+circQueueT<32, 4> sensorData;
 
 
 void loop()
@@ -36,56 +26,60 @@ void loop()
 	readSensor();
 
 
-	delay(500);
+	delay(2000);
 }
 
 
 bool readSensor()
 {
+	// flush it
+	sensorData.reset();
 	// this should check or it will wdt
-	while (dyp.available() < serialData.size)
+	while (dyp.available() < sensorData.space())
 	{
-		Serial.printf(".");
+		Serial.printf("#");
 		delay(100);
 	}
 
-	// read as much as we can
-	int canBeRead = serialData.size - serialData.writeCursor;
-	int actualRead = dyp.readBytes(&serialData.buffer[serialData.writeCursor], canBeRead);
-
-	serialData.writeCursor += actualRead;
-	// we need at least 4 bytes
-	if (serialData.writeCursor > 3)
+	while (dyp.available() && sensorData.space())
 	{
-		int byteStep = 1;
-		int readCursor = 0;
-		for (int examine = 0; examine < serialData.writeCursor - 3; examine += byteStep)
+		sensorData.write(dyp.read());
+	}
+
+	// we need at least 4 bytes
+	bool syncFound = false;
+	for (int examine = 0; !syncFound && (sensorData.available()>3); examine++)
+	{
+
+		if ((sensorData.peek(0)==0xff) && (((sensorData.peek(0) + sensorData.peek(1) + sensorData.peek(2)) & 0xff) == sensorData.peek(3)))
 		{
-			// chksum test
-			if (((serialData.buffer[examine] + serialData.buffer[examine + 1] + serialData.buffer[examine + 2]) & 0xff) == serialData.buffer[examine + 3])
-			{
-				// woot - locked
-				byteStep = 4;
-				readCursor += 4;
-
-				int value = ((serialData.buffer[examine + 1]) << 8) | (serialData.buffer[examine + 2]);
-
-				Serial.printf("%3d. [ %02x %02x %02x %02x ] - %d\n\r", examine, serialData.buffer[examine], serialData.buffer[examine + 1], serialData.buffer[examine + 2], serialData.buffer[examine + 3], value);
-			}
-			else
-			{
-				// boo
-				byteStep = 1;
-				readCursor += 1;
-
-				Serial.println("!");
-			}
+			// we have a hit
+			syncFound = true;
+			// we'll fall out at te next loop condition check
 		}
-		memmove(&serialData.buffer[0], &serialData.buffer[readCursor], serialData.size - readCursor);
-		serialData.writeCursor -= readCursor;
+		else
+		{
+			// read past it
+			sensorData.read();
+		}
+	}
+	if (syncFound)
+	{
+		for (int examine = 0; sensorData.available()>3; examine ++)
+		{
+			// 0 is FF, 1 & 2 we want, # is a checksum
+			byte c1 = sensorData.read();
+			byte c2 = sensorData.read();
+			byte c3 = sensorData.read();
+			byte c4 = sensorData.read();
+			int value = (c2 << 8) | (c3);
+			Serial.printf("%02x %02x %02x %02x - sensor %d - avail %d \n\r",c1,c2,c3,c4,value, sensorData.available());
+		}
+		Serial.println("\n\r----------------------------");
+		return true;
 	}
 
 	Serial.println(":");
 
-
+	return false;
 }
